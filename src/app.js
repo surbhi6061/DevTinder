7d98bd9863fc50275c287f47144a2de2396e29c8
Surbhi <surbhi.k@satorixr.com>
2025-02-13 15:24:40 +0530

    multiple router with same name functionality

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express from 'express'
 
 const app=express();
-app.use("/",(req,res)=>{
-    res.send("hello from the dashboard")
+
+// This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.json({firstName:"surbhi",lastName:"kumari"})
+
 })
+
+//this will match all the HTTP method API calls to /
+// app.use("/",(req,res)=>{
+//     res.send("hello from the dashboard")
+// })
 app.use("/test",(req,res)=>{
     res.send("hello from the server")
 })
 
-app.listen(3000,()=>{
+app.listen(4000,()=>{
     console.log("server started succesfully")
 });
